91b8a5a9d27abdb5b211e4aeba9a4e458ba146a7
Koroliuk <koroliuk.yaroslav@gmail.com>
2022-05-02 11:54:48 +0300

    Lab3

---
+++ Lab3/app/src/main/java/com/example/lab3/fragment/AlertFragment.kt
@@ -0,0 +1,20 @@
+package com.example.lab3.fragment
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import com.example.lab3.R
+
+class AlertFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        AlertDialog.Builder(requireContext())
+            .setMessage(getString(R.string.dialog_message))
+            .setPositiveButton(getString(R.string.button_ok)) { _, _ -> }
+            .create()
+
+    companion object {
+        const val TAG = "AlertDialog"
+    }
+}
